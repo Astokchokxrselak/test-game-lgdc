0374a00339ab0b77b9e0ac9ec53fdeaac5c3537d
Astokchokxrselak <ehinowenuwa00@gmail.com>
2026-09-22 10:50:04 -0400

    add charactercontroller.cs, move function
    also change rygoscharacter hierarchy to parent a characterbody

---
+++ CharacterController.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class CharacterController : CharacterBody2D
+{
+	public static float CharacterDefaultSpeed = 100f;  // in px/sec
+													   // Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public void Move(Vector2 direction, float speed)
+	{
+		Velocity = direction * speed;
+		MoveAndSlide();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
