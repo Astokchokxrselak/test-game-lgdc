0c63448ec80cb36014b944523be1c7792669b23d
Astokchokxrselak <ehinowenuwa00@gmail.com>
2026-09-22 12:28:35 -0400

    add overworldAI types, overworldAIController, gameManager

--- assets/scripts/overworld/CharacterController.cs
+++ assets/scripts/overworld/CharacterController.cs
@@ -3,10 +3,13 @@ using System;
 
 public partial class CharacterController : CharacterBody2D
 {
+	[Export]
+	public OverworldAIType AIType = OverworldAIType.Static;
 	public static float CharacterDefaultSpeed = 100f;  // in px/sec
 													   // Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 	}
 
 	public void Move(Vector2 direction, float speed)
@@ -16,7 +19,8 @@ public partial class CharacterController : CharacterBody2D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		OverworldAIController.UpdateCharacterAI(this, AIType, delta);
 	}
 }
---
+++ assets/scripts/overworld/OverworldAI.cs
@@ -0,0 +1,73 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public enum OverworldAIType
+{
+    Static,
+    RandomWalk,
+    Player
+}
+public abstract class OverworldAI
+{
+    public abstract void UpdateAI(CharacterController character, double delta);
+}
+public class StaticAI : OverworldAI
+{
+    public override void UpdateAI(CharacterController character, double delta)
+    {
+        // Do nothing, character remains static
+    }
+}
+public class RandomWalkAI : OverworldAI
+{
+    public override void UpdateAI(CharacterController character, double delta)
+    {
+        // Implement random walk logic
+    }
+}
+public class PlayerAI : OverworldAI
+{
+    private void HandleInput(CharacterController character, double delta)
+    {
+        Vector2 inputDirection = Vector2.Zero;
+
+        if (Input.IsActionPressed("ui_right"))
+            inputDirection.X += 1;
+        if (Input.IsActionPressed("ui_left"))
+            inputDirection.X -= 1;
+        if (Input.IsActionPressed("ui_down"))
+            inputDirection.Y += 1;
+        if (Input.IsActionPressed("ui_up"))
+            inputDirection.Y -= 1;
+
+        if (inputDirection != Vector2.Zero)
+        {
+            inputDirection = inputDirection.Normalized();
+            character.Move(inputDirection, CharacterController.CharacterDefaultSpeed);
+        }
+    }
+    public override void UpdateAI(CharacterController character, double delta)
+    {
+        HandleInput(character, delta);
+    }
+}
+public static class OverworldAIController
+{
+    public static Dictionary<OverworldAIType, OverworldAI> AIDictionary = new Dictionary<OverworldAIType, OverworldAI>();
+    public static void Initialize()
+    {
+        AIDictionary[OverworldAIType.Static] = new StaticAI();
+        AIDictionary[OverworldAIType.RandomWalk] = new RandomWalkAI();
+        AIDictionary[OverworldAIType.Player] = new PlayerAI();
+    }
+    public static OverworldAI GetAI(OverworldAIType type)
+    {
+        return AIDictionary[type];
+    }
+    public static void UpdateCharacterAI(CharacterController character, OverworldAIType type, double delta)
+    {
+        OverworldAI ai = GetAI(type);
+        ai.UpdateAI(character, delta);
+    }
+}
